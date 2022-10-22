c1cc413ad00282728efb0d4a106292291688bc49
Updesh Pr. Singh <updesh071@gmail.com>
2022-10-23 00:48:16 +0530

    late night update

---
+++ Assets/Scrpits/VehocleController.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class VehocleController : MonoBehaviour
+{
+    public Rigidbody2D Ftire;
+    public Rigidbody2D Btire;
+    public float speed;
+    public float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Btire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        Ftire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        
+    }
+}
