1ea2d764ae1b31ff79c33c2a08889c6981d5e8ae
Updesh Pr. Singh <updesh071@gmail.com>
2022-11-02 02:31:11 +0530

    game

---
+++ Login/Assets/Scripts/PlayFabMar.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+
+public class PlayFabMar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    // Update is called once per frame
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successful Login/account create!");
+        //GetAppearance();
+    }
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error While Logging in /account create!");
+        Debug.Log(error.GenerateErrorReport());
+    }
+    // Send data to LeaderBoad Pass the value
+    public void SendLeaderBoad(int score)
+    {
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName ="PlatformScore",
+                    Value =score
+                }
+            }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderBoardUpdate, OnError);
+    }
+
+    void OnLeaderBoardUpdate(UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log("Successfull Leaderboad sent");
+    }
+
+    // Get data of LeaderBoad
+    public void GetLeaderboard()
+    {
+        var request = new GetLeaderboardRequest
+        {
+            StatisticName = "PlatformScore",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+    void OnLeaderboardGet(GetLeaderboardResult result)
+    {
+        foreach (var item in result.Leaderboard)
+        {
+            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+
+        }
+    }
+
+    //Save Appearance
+    /* public void SaveAppearance()
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string> {
+                {"Hat",CharcterEditor.Hat},
+                {}
+            }
+        };
+        PlayFabClientAPI.UpdateUserData(request, OnDataSend, OnError);
+    }
+    void OnDataSend(UpdateUserDataResult result)
+    {
+        Debug.Log("Successful user data save");
+    }
+    */
+
+    /*public void GetAppearance()
+    {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), OnDataRecieved, OnError);
+    }
+    void OnDataRecieved(GetUserDataResult result)
+    {
+        Debug.Log("Recieved user data!");
+        if (result.Data != null && result.Data.ContainsKey("Hat") && result.Data.ContainsKey("Skin") && result.Data.ContainsKey("Beard"))
+        {
+            characterEditor.SetAppearance(result.Data["Hat"].Value, result.Data["Skin"].Value, result.Data["Beard"].Value);
+        }
+        else
+        {
+            Debug.Log("Player data not complete!");
+        }
+    }*/
+
+
+}
