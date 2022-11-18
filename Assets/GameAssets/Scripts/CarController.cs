9fa464ab4b613356a309e3a7de1d64d6789e34a2
Updesh Pr. Singh <updesh071@gmail.com>
2022-11-19 00:09:18 +0530

    change

--- Assets/GameAssets/Scripts/CarController.cs
+++ Assets/GameAssets/Scripts/CarController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float fuel = 1;
+    public float fuelconsumption = 0.1f;
+    public Rigidbody2D carRigidbody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 20;
+    public float carTorque = 10;
+    private float movement;
+    public UnityEngine.UI.Image image;
     void Update()
     {
+        movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
+    }
+    private void FixedUpdate()
+    {
+        if(fuel >0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
         
+
+        fuel -= fuelconsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
+
     }
 }
